58ae70a66a77f0bcf6d318cd980e447b2745cf74
ByeongeonPark <bang3453@ajou.ac.kr>
2025-05-20 03:35:27 +0900

    [AJO-21(25)_사이드바 구현 및 배너페이지 연결 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthForm } from '@/components/auth-form'
 import LoginPage from "@/pages/LoginPage.jsx";
 import MainPage from "@/pages/Mainpage.jsx";
-// import { AuthorityPage } from '@/pages/AuthorityPage'
-
+import BannerManagePage from "@/pages/BannerManagePage.jsx";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                {<Route path="/admin" element={<MainPage />} />}
+                <Route path="/admin" element={<MainPage />} />
+                <Route path="/admin/banner-manage" element={<BannerManagePage />} />
             </Routes>
         </Router>
     )
---
+++ src/pages/BannerManagePage.jsx
@@ -0,0 +1,7 @@
+// src/pages/BannerManagePage.jsx
+
+function BannerManagePage() {
+    return <div>배너 관리 페이지입니다</div>;
+}
+
+export default BannerManagePage;  // ✅ 이 줄이 반드시 필요합니다
