83fd054916130ed5d9311ea47f33b1e3df8a59fa
minjoonJ <breauxchristoper@gmail.com>
2025-05-27 15:42:41 +0900

    refactor: 인증 방식 변경 쿠키 -> 세션스토어

--- src/api/member.js
+++ src/api/member.js
@@ -1,21 +1,21 @@
 // src/api/member.js
-import { axiosInstance } from "./axiosInstance";
+import { adminAxios } from "./adminAxios.js";
 
 // 1. 멤버 목록 조회
 export const fetchMembersByRole = async () =>
-    axiosInstance.get(`/admin/members`);
+    adminAxios.get(`/members`);
     // axiosInstance.get(`/admin/members?role=${role}`);
 
 // 2. 역할 변경
 export const updateMemberRole = async (id, role) =>
-    axiosInstance.patch(`/admin/members/${id}/role`, { role });
+    adminAxios.patch(`/members/${id}/role`, { role });
 
 // 3. 권한 부여
 export const grantPermission = async (id, permission) =>
-    axiosInstance.post(`/admin/members/${id}/permissions`, { permission });
+    adminAxios.post(`/members/${id}/permissions`, { permission });
 
 // 4. 권한 회수
 export const revokePermission = async (id, permission) =>
-    axiosInstance.delete(`/admin/members/${id}/permissions`, {
+    adminAxios.delete(`/members/${id}/permissions`, {
         data: { permission },
     });
